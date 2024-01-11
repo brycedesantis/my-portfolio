cfb612aa170fe791ca4b55faa7a38fe235b7be8d
Bryce DeSantis <potreader@hotmail.com>
2024-01-11 11:34:09 -0500

    Add and style nav element

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import Nav from "./Components/Nav"
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+	const [darkMode, setDarkMode] = useState(true)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	function toggleDarkMode() {
+		setDarkMode((prevMode) => !prevMode)
+	}
+
+	return (
+		<div className={`h-lvh w-full mx-auto ${darkMode ? "dark" : "light"}`}>
+			<Nav darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+		</div>
+	)
 }
-
-export default App
