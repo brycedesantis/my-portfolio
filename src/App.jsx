baf1562448a0e5b72e87070d0001dfc7e3b8c36b
Bryce DeSantis <potreader@hotmail.com>
2024-01-19 13:37:21 -0500

    Restyle about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import Nav from "./Components/Nav"
 import Home from "./Components/Home"
+import About from "./Components/About"
 
 export default function App() {
 	const [darkMode, setDarkMode] = useState(true)
@@ -13,6 +14,7 @@ export default function App() {
 		<div className={`${darkMode ? "dark" : "light"}`}>
 			<Nav darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
 			<Home />
+			<About />
 		</div>
 	)
 }
